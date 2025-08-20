19f188a4af06308b827e22dc6722b36b7dbcc5e2
Tigran <mherannas@gmail.com>
2025-08-20 18:02:37 +0400

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { MainBoard } from "./game/components/MainBoard";
 
 function App() {
-  return <MainBoard />;
+  return (
+    <Routes>
+      <Route path="/" element={<MainBoard />} />
+    </Routes>
+  );
 }
 
 export default App;
