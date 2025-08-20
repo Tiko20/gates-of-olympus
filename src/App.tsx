d3be94e6e4c6e9b3fbb64127332dad031dbe6645
Tigran <mherannas@gmail.com>
2025-08-20 18:34:57 +0400

    uninstall react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { MainBoard } from "./game/components/MainBoard";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<MainBoard />} />
-    </Routes>
-  );
+  return <MainBoard />;
 }
 
 export default App;
